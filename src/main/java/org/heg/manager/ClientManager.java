b776cccb6b53cd1a08cd42b8a107c284198df2f3
Frederic Mencier <fmencier@yahoo.fr>
2025-04-30 11:33:41 +0200

    rename var

--- src/main/java/org/heg/manager/ClientManager.java
+++ src/main/java/org/heg/manager/ClientManager.java
@@ -8,13 +8,13 @@ public class ClientManager {
 
     private static final Logger LOG = Logger.getLogger(ClientManager.class);
 
-    private final Base64Encoder base64Encoder = new Base64Encoder();
+    private final Base64Encoder encoder = new Base64Encoder();
 
     public ClientEntity createClient(final String firstname, final String lastname) {
         ClientEntity clientEntity = new ClientEntity();
         clientEntity.setClientId(1L);
-        clientEntity.setFirstname(base64Encoder.encodeData(firstname));
-        clientEntity.setLastname(base64Encoder.encodeData(lastname));
+        clientEntity.setFirstname(encoder.encodeData(firstname));
+        clientEntity.setLastname(encoder.encodeData(lastname));
         return clientEntity;
     }
 }
